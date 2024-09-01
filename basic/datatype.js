b3511579172b84c67397ea80bd5c768e4bf2a085
biyalas <130562162+biyalas@users.noreply.github.com>
2024-09-01 11:16:35 +0000

    commit

--- basic/datatype.js
+++ basic/datatype.js
@@ -7,5 +7,16 @@ let isLogIn = false
 
 /* bigint, string, boolean, number(range), null(standalone value), undefined(unasssigned value), symbol(unique) */
 
-console.log(typeof null);  //shows type of data value//
+// console.log(typeof null);  //shows type of data value//
+
+//non primitive datatype or refrence datatype
+let marvel = ["thor","iornman","captain_america"]; //array
+let obj = {                //object
+    me : "sneha",
+    age : 20,
+}
+ //function
+const myFunction = function(){
+    console.log("Hello world");
+}
 
