113d9859d6b88cd3cba3dc50558229fd2aad160d
biyalas <130562162+biyalas@users.noreply.github.com>
2024-09-01 10:05:18 +0000

    commit

---
+++ basic/operation.js
@@ -0,0 +1,19 @@
+let score = "456"
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+/* 
+"456" = 456
+"456sd" = NaN
+1 = true, "" = false, "sneha" = true
+*/
+
+
+
+
