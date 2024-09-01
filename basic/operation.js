cd4e4e55bfeadb93c238738e9ea910e1088fda3a
biyalas <130562162+biyalas@users.noreply.github.com>
2024-09-01 10:50:59 +0000

    commit

--- basic/operation.js
+++ basic/operation.js
@@ -1,18 +1,39 @@
 let score = "456"
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 /* 
 "456" = 456
 "456sd" = NaN
 1 = true, "" = false, "sneha" = true
 */
+
+//operations//
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+let first = "sneha"
+let last = " biyala"
+let name = first + last
+// console.log(name); 
+
+// console.log("1" + 2 + 2);  //if starting is with string then all will be taken as string
+// console.log(1 + 2 + "2"); //if starting is not a string then addition will happen and then adds the last string element to it
+
+let counter = 100
+++counter;
+// console.log(counter);
+
+
+
 
 
 
