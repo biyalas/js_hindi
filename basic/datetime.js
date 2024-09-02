63565522498900f3b480fcfd482f15bc759052c2
biyalas <130562162+biyalas@users.noreply.github.com>
2024-09-02 18:07:07 +0000

    commit

---
+++ basic/datetime.js
@@ -0,0 +1,32 @@
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+
+// let date = new Date(2024,8,2) //months starts from 0 till 11
+// console.log(date.toDateString());
+
+// let date = new Date(2024,8,2,11,18)
+// console.log(date.toLocaleString());
+
+// let date = new Date("2024-09-2")
+// console.log(date.toLocaleString());
+
+// let timestamp = Date.now()
+// console.log(timestamp);  //time commes in milisceonnd
+// console.log(date.getTime());
+// console.log(date.now()/1000);  //converts in seconds
+
+// let newDate = new Date(2024,9,2,11,34)
+// console.log(newDate.getMonth());
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
+
+
+
+
+
